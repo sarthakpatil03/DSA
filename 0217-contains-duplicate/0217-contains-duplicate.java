6b42cbd0becc3f363d4717995fdd06d7a721b4fb
Sarthak Patil <sarthakpatil0345@gmail.com>
2026-08-23 10:09:16 +0530

    Time: 17 ms (85.74%), Space: 108.2 MB (36.25%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set=new HashSet<>();
+        for(int val:nums) {
+            if(set.contains(val)) {
+                return true;
+            }
+            set.add(val);
+        }
+        return false;
+    }
+}
